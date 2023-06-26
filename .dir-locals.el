0c370a6e772735efa63c6467a403786ada87a256
alexpanter <alex_c007@hotmail.com>
2023-06-26 12:29:18 +0200

    Maybe it now works with dir-locals and Emacs semantic

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+((nil . ((company-clang-arguments . ("-I/home/gg/dev/vclab-vtek/include/vtek/"
+                                     "-I/home/gg/dev/vclab-vtek/external/spdlog/"))
+		 (company-c-headers-path-user . ("-I/home/gg/dev/vclab-vtek/include/vtek/"
+										 "-I/home/gg/dev/vclab-vtek/external/spdlog/"))
+		 (eval . (message "Loading .dir-locals.el"))
+		 (eval . (semantic-add-system-include . ("/home/gg/dev/vclab-vtek/include/vtek/")))
+		 )
+
+	  ))
