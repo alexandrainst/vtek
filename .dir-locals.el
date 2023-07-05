7eec76e19b3f18eaf0ba252958e35a96e7f42694
alexpanter <alex_c007@hotmail.com>
2023-07-05 17:27:16 +0200

    Default .dir-locals just for emacs dev

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,9 @@
-((nil . ((company-clang-arguments . ("-I/home/gg/dev/vclab-vtek/include/vtek/"
-                                     "-I/home/gg/dev/vclab-vtek/external/spdlog/"))
-		 (company-c-headers-path-user . ("-I/home/gg/dev/vclab-vtek/include/vtek/"
-										 "-I/home/gg/dev/vclab-vtek/external/spdlog/"))
+((nil . ((company-clang-arguments . ("-I<path/to/vtek/>include/vtek/"
+                                     "-I<path/to/vtek/>external/spdlog/"))
+		 (company-c-headers-path-user . ("-I<path/to/vtek/>include/vtek/"
+										 "-I<path/to/vtek/>external/spdlog/"))
 		 (eval . (message "Loading .dir-locals.el"))
-		 (eval . (semantic-add-system-include . ("/home/gg/dev/vclab-vtek/include/vtek/")))
+		 (eval . (semantic-add-system-include . ("<path/to/vtek/>include/vtek/")))
 		 )
 
 	  ))
