8c17f1aac4a931c616ba2392b02a17d7ff0e2878
alexpanter <alex_c007@hotmail.com>
2023-08-29 15:42:04 +0200

    Add unit test setup

---
+++ tests/test_camera.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is test_camera.cpp\n";
+	return 0;
+}
---
+++ tests/test_formats.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "This is test_formats.cpp\n";
+	return 0;
+}
