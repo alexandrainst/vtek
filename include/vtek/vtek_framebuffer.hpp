a0b7323fbe1ee79c1272adcb9f6a86c499ce1cfb
alexpanter <alex_c007@hotmail.com>
2023-10-26 09:10:25 +0200

    Framebuffer type in objects header

--- include/vtek/vtek_framebuffer.hpp
+++ include/vtek/vtek_framebuffer.hpp
@@ -1,24 +1,32 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vector>
+
 #include "vtek_device.hpp"
+#include "vtek_glm_includes.hpp"
+#include "vtek_object_handles.hpp"
 
 
 namespace vtek
 {
-	struct FramebufferCreateInfo
+	enum class AttachmentType
 	{
-
+		color, depth, depth_stencil
 	};
 
-	struct Framebuffer; // opaque handle
+	struct FramebufferAttachment
+	{
+		AttachmentType type {AttachmentType::color};
+	};
 
+	struct FramebufferInfo
+	{
+		std::vector<FramebufferAttachment> attachments;
+		glm::uvec2 resolution {1,1};
+	};
 
 	Framebuffer* framebuffer_create(
-		const FramebufferCreateInfo* info, Device* device);
+		const FramebufferInfo* info, Device* device);
 	void framebuffer_destroy(Framebuffer* framebuffer);
-
-	// TODO: What is actually needed/desired?
-	std::vector<Framebuffer*> framebuffer_create_multiple();
-	std::vector<Framebuffer*> framebuffer_create_for_swapchain();
 }
